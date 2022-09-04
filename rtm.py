b7e3a1ee90ebb569ce58b7143ec38204dbbdc55f
zzzzzx-1115 <xinzheng1115@gmail.com>
2022-09-04 21:10:19 +0800

    2222

--- rtm.py
+++ rtm.py
@@ -1,6 +1,6 @@
-import subprocess
+import os
 import argparse
-
+import glob
 
 
 #python rtmscore.py -p ./1qkt_p.pdb -l ./1qkt_decoys.sdf -rl ./1qkt_l.sdf -gen_pocket -c 10.0 -m ../trained_models/rtmscore_model1.pth
@@ -8,9 +8,21 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='rtm')
-parser.add_argument('--protein', '-p', default='')
-parser.add_argument('--ligand', '-l', default='')  #ligand files, 匹配~/VINA_screening/10gs/output/testxxx下所有ligand.pdb
-#首先要把ligand.pdb全部转换成sdf
+parser.add_argument('--protein', '-p', default='/home/ubuntu/VINA_creening/example/', help='prefix of protein')
+#protein path: ~/VINA_creening/example/{name}_protein.pdb
+parser.add_argument('--ligand', '-l', default='/home/ubuntu/output/', help='prefix of ligands')
+#ligand path: ~/output/{name}/{name}_ligand_out_{i}.sdf
+parser.add_argument('--name', '-n', required=True, help='name of complex')
+parser.add_argument('--rtm_loc', default='/home/ubuntu/RTMScore/example/rtmscore.py')
+parser.add_argument('--out', '-o', default='/home/ubuntu/output_rtm/', help='prefix of output')
 
 
+args = parser.parse_args()
 
+
+if __name__ == '__main__':
+    ligands = glob.glob(os.path.join(args.ligand, args.name, '*.sdf'))
+    outpath = os.path.join(args.out, args.name + '.csv')
+    for ligand in ligands:
+        os.system('python' + args.rtm_loc + '-p' + os.path.join(args.protein, '{}_protein.pdb'.format(args.name))
+              + '-l' + ligand + '-o' + outpath)
