989498eb4ecc11ae38c59176516399e3fd403bd8
zzzzzx-1115 <xinzheng1115@gmail.com>
2022-09-04 11:02:21 +0800

    2222

--- pdb2sdf.py
+++ pdb2sdf.py
@@ -1,7 +1,8 @@
-import openbabel
+from openbabel import openbabel
 import argparse
 import os
 import glob
+import tqdm
 
 parser = argparse.ArgumentParser(description='converter')
 parser.add_argument('--file', '-f', required=True)
@@ -12,12 +13,11 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     conv = openbabel.OBConversion()
-    for i in range(1, 11):
+    for i in tqdm(range(1, 11)):
         path = os.path.join(args.file, 'output', 'test_{}'.format(i))
         files = glob.glob(os.path.join(path, '????_ligand_out*.pdb'))
         for file in files:
             name = os.path.basename(file)
-            print(name)
             outfile = os.path.join(args.output, 'output', name[:4])
             if not os.path.exists(outfile):
                 os.makedirs(outfile)
